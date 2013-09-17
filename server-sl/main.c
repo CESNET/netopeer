972ca6102bd2d7df4499245bc347e68c29243453
Radek Krejci <rkrejci@cesnet.cz>
2013-09-17 14:38:41 +0200

    server-sl: INIT: Initial commit of the single-level server
    Cloned from the libnetconf project.

---
+++ server-sl/main.c
@@ -0,0 +1,375 @@
+/*
+ * netopeer-server-sl (main.c)
+ * Author Radek Krejci <rkrejci@cesnet.cz>
+ *
+ * Example implementation of event-driven NETCONF server using libnetconf.
+ *
+ * Copyright (C) 2012-2013 CESNET, z.s.p.o.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of the Company nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * ALTERNATIVELY, provided that this notice is retained in full, this
+ * product may be distributed under the terms of the GNU General Public
+ * License (GPL) version 2 or later, in which case the provisions
+ * of the GPL apply INSTEAD OF those given above.
+ *
+ * This software is provided ``as is, and any express or implied
+ * warranties, including, but not limited to, the implied warranties of
+ * merchantability and fitness for a particular purpose are disclaimed.
+ * In no event shall the company or contributors be liable for any
+ * direct, indirect, incidental, special, exemplary, or consequential
+ * damages (including, but not limited to, procurement of substitute
+ * goods or services; loss of use, data, or profits; or business
+ * interruption) however caused and on any theory of liability, whether
+ * in contract, strict liability, or tort (including negligence or
+ * otherwise) arising in any way out of the use of this software, even
+ * if advised of the possibility of such damage.
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <syslog.h>
+
+#ifndef DISABLE_NOTIFICATIONS
+#	include <pthread.h>
+#endif
+
+#include <event2/event.h>
+
+#include <libnetconf.h>
+
+static const char rcsid[] __attribute__((used)) ="$Id: "__FILE__": "RCSID" $";
+
+#ifdef __GNUC__
+#  define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
+#else
+#  define UNUSED(x) UNUSED_ ## x
+#endif
+
+struct srv_config {
+	struct nc_session *session;
+	ncds_id dsid;
+	struct event_base *event_base;
+	struct event *event_input;
+};
+
+struct ntf_thread_config {
+	struct nc_session *session;
+	nc_rpc *subscribe_rpc;
+};
+
+void clb_print(NC_VERB_LEVEL level, const char* msg)
+{
+
+	switch (level) {
+	case NC_VERB_ERROR:
+		syslog(LOG_ERR, "E| %s", msg);
+		break;
+	case NC_VERB_WARNING:
+		syslog(LOG_WARNING, "W| %s", msg);
+		break;
+	case NC_VERB_VERBOSE:
+		syslog(LOG_INFO, "V| %s", msg);
+		break;
+	case NC_VERB_DEBUG:
+		syslog(LOG_DEBUG, "D| %s", msg);
+		break;
+	}
+}
+
+void print_version()
+{
+	fprintf(stdout, "Netopeer server - single-level architecture\n");
+	fprintf(stdout, "version     : %s\n", VERSION);
+	fprintf(stdout, "compile time: %s, %s\n", __DATE__, __TIME__);
+}
+
+#ifndef DISABLE_NOTIFICATIONS
+void* notification_thread(void* arg)
+{
+	struct ntf_thread_config *config = (struct ntf_thread_config*)arg;
+
+	ncntf_dispatch_send(config->session, config->subscribe_rpc);
+	nc_rpc_free(config->subscribe_rpc);
+	free(config);
+
+	return (NULL);
+}
+#endif /* DISABLE_NOTIFICATIONS */
+
+void process_rpc(evutil_socket_t UNUSED(in), short UNUSED(events), void *arg)
+{
+	nc_rpc *rpc = NULL;
+	nc_reply *reply = NULL;
+	NC_RPC_TYPE req_type;
+	NC_OP req_op;
+	struct nc_err *e;
+	int ret;
+	struct srv_config *config = (struct srv_config*)arg;
+
+#ifndef DISABLE_NOTIFICATIONS
+	struct ntf_thread_config *ntf_config = NULL;
+	pthread_t thread;
+#endif
+
+	/* receive incoming message */
+	ret = nc_session_recv_rpc(config->session, -1, &rpc);
+	if (ret != NC_MSG_RPC) {
+		switch(ret) {
+		case NC_MSG_NONE:
+			/* the request was already processed by libnetconf or no message available */
+			return;
+		case NC_MSG_UNKNOWN:
+			if (nc_session_get_status(config->session) != NC_SESSION_STATUS_WORKING) {
+				/* something really bad happend, and communication os not possible anymore */
+				event_base_loopbreak(config->event_base);
+			}
+			return;
+		default:
+			return;
+		}
+	}
+
+	/* process it */
+	req_type = nc_rpc_get_type(rpc);
+	req_op = nc_rpc_get_op(rpc);
+	if (req_type == NC_RPC_SESSION) {
+		/* process operations affectinf session */
+		switch(req_op) {
+		case NC_OP_CLOSESESSION:
+			/* exit the event loop immediately without processing any following request */
+			reply = nc_reply_ok();
+			event_base_loopbreak(config->event_base);
+			break;
+		case NC_OP_KILLSESSION:
+			/* todo: kill the requested session */
+			reply = nc_reply_error(nc_err_new(NC_ERR_OP_NOT_SUPPORTED));
+			break;
+#ifndef DISABLE_NOTIFICATIONS
+		case NC_OP_CREATESUBSCRIPTION:
+			if (nc_cpblts_enabled(config->session, "urn:ietf:params:netconf:capability:notification:1.0") == 0) {
+				reply = nc_reply_error(nc_err_new(NC_ERR_OP_NOT_SUPPORTED));
+				break;
+			}
+
+			/* check if notifications are allowed on this session */
+			if (nc_session_notif_allowed(config->session) == 0) {
+				clb_print(NC_VERB_ERROR, "Notification subscription is not allowed on this session.");
+				e = nc_err_new(NC_ERR_OP_FAILED);
+				nc_err_set(e, NC_ERR_PARAM_TYPE, "protocol");
+				nc_err_set(e, NC_ERR_PARAM_MSG, "Another notification subscription is currently active on this session.");
+				reply = nc_reply_error(e);
+				break;
+			}
+
+			reply = ncntf_subscription_check(rpc);
+			if (nc_reply_get_type (reply) != NC_REPLY_OK) {
+				break;
+			}
+
+			if ((ntf_config = malloc(sizeof(struct ntf_thread_config))) == NULL) {
+				clb_print(NC_VERB_ERROR, "Memory allocation failed.");
+				e = nc_err_new(NC_ERR_OP_FAILED);
+				nc_err_set(e, NC_ERR_PARAM_MSG, "Memory allocation failed.");
+				reply = nc_reply_error(e);
+				e = NULL;
+				break;
+			}
+			ntf_config->session = config->session;
+			ntf_config->subscribe_rpc = nc_rpc_dup(rpc);
+
+			/* perform notification sending */
+			if ((ret = pthread_create(&thread, NULL, notification_thread, ntf_config)) != 0) {
+				nc_reply_free(reply);
+				e = nc_err_new(NC_ERR_OP_FAILED);
+				nc_err_set(e, NC_ERR_PARAM_MSG, "Creating a thread for sending Notifications failed.");
+				reply = nc_reply_error(e);
+				e = NULL;
+			}
+			pthread_detach(thread);
+			break;
+#endif
+		default:
+			reply = nc_reply_error(nc_err_new(NC_ERR_OP_NOT_SUPPORTED));
+			break;
+		}
+	} else if (req_type == NC_RPC_DATASTORE_READ) {
+		/* process operations reading datastore */
+		switch (req_op) {
+		case NC_OP_GET:
+		case NC_OP_GETCONFIG:
+		case NC_OP_GETSCHEMA:
+		case NC_OP_VALIDATE:
+			reply = ncds_apply_rpc2all(config->session, rpc,  NULL);
+			break;
+		default:
+			reply = nc_reply_error(nc_err_new(NC_ERR_OP_NOT_SUPPORTED));
+			break;
+		}
+	} else if (req_type == NC_RPC_DATASTORE_WRITE) {
+		/* process operations affecting datastore */
+		switch (req_op) {
+		case NC_OP_LOCK:
+		case NC_OP_UNLOCK:
+		case NC_OP_COPYCONFIG:
+		case NC_OP_DELETECONFIG:
+		case NC_OP_EDITCONFIG:
+		case NC_OP_COMMIT:
+		case NC_OP_DISCARDCHANGES:
+			reply = ncds_apply_rpc2all(config->session, rpc, NULL);
+			break;
+		default:
+			reply = nc_reply_error(nc_err_new(NC_ERR_OP_NOT_SUPPORTED));
+			break;
+		}
+	} else {
+		/* process other operations */
+		reply = ncds_apply_rpc2all(config->session, rpc, NULL);
+	}
+
+	/* create reply */
+	if (reply == NULL) {
+		reply = nc_reply_error(nc_err_new(NC_ERR_OP_FAILED));
+	} else if (reply == NCDS_RPC_NOT_APPLICABLE) {
+		e = nc_err_new(NC_ERR_OP_FAILED);
+		nc_err_set(e, NC_ERR_PARAM_MSG, "Requested operation cannot be performed on the managed datastore.");
+		reply = nc_reply_error(e);
+	}
+
+	/* and send the reply to the client */
+	nc_session_send_reply(config->session, rpc, reply);
+	nc_rpc_free(rpc);
+	nc_reply_free(reply);
+
+	/* and run again when a next message comes */
+}
+
+int main(int UNUSED(argc), char** UNUSED(argv))
+{
+	struct srv_config config;
+	struct ncds_ds* datastore;
+	int init;
+
+	/* set verbosity and function to print libnetconf's messages */
+	nc_verbosity(NC_VERB_DEBUG);
+
+	/* set message printing into the system log */
+	openlog("ncserver", LOG_PID, LOG_DAEMON);
+	nc_callback_print(clb_print);
+
+	init = nc_init(NC_INIT_ALL);
+	if (init == -1) {
+		clb_print(NC_VERB_ERROR, "libnetconf initiation failed.");
+		return (EXIT_FAILURE);
+	}
+
+	/* if you want to change default behavior of libnetconf's with-default capability :*/
+	/*
+	 * ncdflt_set_basic_mode(NCWD_MODE_ALL);
+	 */
+
+	/* prepare configuration datastore */
+	datastore = ncds_new_transapi(NCDS_TYPE_FILE, SERVERCFG_PATH"/toaster.yin", SERVERCFG_PATH"/toaster.so");
+	if (datastore == NULL) {
+		clb_print(NC_VERB_ERROR, "Datastore preparing failed.");
+		return (EXIT_FAILURE);
+	}
+	if (ncds_file_set_path(datastore, SERVERCFG_PATH"/datastore.xml") != 0) {
+		clb_print(NC_VERB_ERROR, "Linking datastore to a file failed.");
+		return (EXIT_FAILURE);
+	}
+	config.dsid = ncds_init(datastore);
+	if (config.dsid <= 0) {
+		ncds_free(datastore);
+		clb_print(NC_VERB_ERROR, "Initiating datastore failed.");
+		return (EXIT_FAILURE);
+	}
+
+	/*
+	 * If needed, add location(s) of other models (e.g. with augment
+	 * statements) using (any combination) of these two functions:
+	 *
+	 * ncds_add_model("./dir/file.yin");
+	 * ncds_add_models_path("./models_dir/");
+	 */
+
+	/*
+	 * Enable supported features
+	 * By default, all features defined in the data models are disabled. To
+	 * change this setting, use following functions:
+	 *
+	 * ncds_feature_enable("module-name", "feature1");
+	 * ncds_feature_disable("module-name", "feature1");
+	 * ncds_features_enableall("module-name");
+	 * ncds_features_disableall("module-name");
+	 */
+
+	/*
+	 * Finish datastore settings
+	 * This should be done before applying any NETCONF operation
+	 * to the datastore.
+	 */
+	if (ncds_consolidate() != EXIT_SUCCESS) {
+		clb_print(NC_VERB_ERROR, "Setting up datastores failed.");
+		nc_close(0);
+		return (EXIT_FAILURE);
+	}
+
+	/* Initialize loaded devices
+	 * if using tranaspi full initialize will be performed
+	 * otherwise only copyconfig (startup->running)
+	 */
+	if (ncds_device_init(NULL, NULL, 0)) {
+		clb_print(NC_VERB_ERROR, "Setting up devices failed.");
+		nc_close(0);
+		return (EXIT_FAILURE);
+	}
+
+	/* create the NETCONF session -- accept incoming connection */
+	config.session = nc_session_accept(NULL);
+	if (config.session == NULL) {
+		clb_print(NC_VERB_ERROR, "Session not established.\n");
+		return (EXIT_FAILURE);
+	}
+
+	/* monitor the session */
+	nc_session_monitor(config.session);
+
+	/* prepare event base (libevent) */
+	config.event_base = event_base_new();
+	if (config.event_base == NULL) {
+		clb_print(NC_VERB_ERROR, "Event base initialisation failed.\n");
+		return (EXIT_FAILURE);
+	}
+
+	config.event_input = event_new(config.event_base, (evutil_socket_t)nc_session_get_eventfd(config.session), EV_READ | EV_PERSIST, process_rpc, (void*) (&config));
+	/* add the event to the event base and run the main event loop */
+	event_add (config.event_input, NULL);
+	event_base_dispatch(config.event_base);
+
+	/* cleanup */
+	event_free(config.event_input);
+	event_base_free(config.event_base);
+	if (nc_session_get_status(config.session) == NC_SESSION_STATUS_WORKING) {
+		nc_session_close(config.session, NC_SESSION_TERM_CLOSED);
+	}
+	nc_session_free(config.session);
+	ncds_free(datastore);
+
+	nc_close(0);
+
+	/* bye, bye */
+	return (EXIT_SUCCESS);
+}
---
+++ server-sl/toaster/toaster.c
@@ -0,0 +1,293 @@
+/*
+* This is automaticaly generated callbacks file
+* It contains 3 parts: Configuration callbacks, RPC callbacks and state data callbacks.
+* Do NOT alter function signatures or any structure untill you exactly know what you are doing.
+*/
+
+#include <stdlib.h>
+#include <libxml/tree.h>
+#include <libnetconf_xml.h>
+#include <pthread.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+/* transAPI version which must be compatible with libnetconf */
+int transapi_version = 2;
+
+/* Determines whether XML arguments are passed as (xmlDocPtr) or (char *). */
+int with_libxml2 = 1;
+
+/*
+ * Signal to libnetconf that configuration data were modified by any callback.
+ * 0 - data not modified
+ * 1 - data have been modified
+ */
+int config_modified = 0;
+
+/* toaster status structure */
+struct toaster_status {
+	int enabled;
+	int toasting;
+	pthread_mutex_t toaster_mutex;
+};
+
+/* status structure instance, stored in shared memory */
+struct toaster_status * status = NULL;
+
+/**
+ * @brief Initialize plugin after loaded and before any other functions are called.
+ *
+ * @return EXIT_SUCCESS or EXIT_FAILURE
+ */
+int transapi_init(void)
+{
+	key_t shmkey;
+	int shmid;
+	int first;
+
+	/* get shared memory key */
+	if ((shmkey = ftok ("/proc/self/exe", 1)) == -1) {
+		return EXIT_FAILURE;
+	}
+
+	if ((shmid = shmget (shmkey, sizeof(struct toaster_status), 0666)) != -1) { /* get id of shared memory if exist */
+		first = 0;
+	} else if ((shmid = shmget (shmkey, sizeof(struct toaster_status), IPC_CREAT | 0666)) != -1) { /* create shared memory */
+		first = 1;
+	} else { /*shared memory can not be found nor created */
+		return EXIT_FAILURE;
+	}
+
+	/* attach shared memory */
+	if ((status = shmat (shmid, NULL, 0)) == (void*)-1) {
+		return EXIT_FAILURE;
+	}
+	/* first run after shared memory removed (reboot, manualy) initiate the mutex */
+	if (first) {
+		if (pthread_mutex_init (&status->toaster_mutex, NULL)) {
+			return EXIT_FAILURE;
+		}
+		status->toasting = 0;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+/**
+ * @brief Free all resources allocated on plugin runtime and prepare plugin for removal.
+ */
+void transapi_close(void)
+{
+	shmdt(status);
+	return;
+}
+/**
+ * @brief Retrieve state data from device and return them as serialized XML *
+ * @param model	Device data model. Serialized YIN.
+ * @param running	Running datastore content. Serialized XML.
+ * @param[out] err	Double poiter to error structure. Fill error when some occurs.
+ *
+ * @return State data as serialized XML or NULL in case of error.
+ */
+char * get_state_data (char * model, char * running, struct nc_err **err)
+{
+	xmlDocPtr state;
+	xmlNodePtr root;
+	xmlNsPtr ns;
+	xmlBufferPtr buf = xmlBufferCreate();
+	char * ret;
+
+	state = xmlNewDoc (BAD_CAST "1.0");
+	root = xmlNewDocNode (state, NULL, BAD_CAST "toaster", NULL);
+	xmlDocSetRootElement (state, root);
+	ns = xmlNewNs (root, BAD_CAST "http://netconfcentral.org/ns/toaster", NULL);
+	xmlNewChild (root, ns, BAD_CAST "toasterManufacturer", BAD_CAST "CESNET, z.s.p.o.");
+	xmlNewChild (root, ns, BAD_CAST "toasterModelNumber", BAD_CAST "toaster");
+	xmlNewChild (root, ns, BAD_CAST "toasterStatus", BAD_CAST (status->enabled ? "down" : "up" ));
+
+	xmlNodeDump (buf, state, root, 0, 0);
+	ret = strdup(xmlBufferContent(buf));
+
+	xmlBufferFree(buf);
+	xmlFreeDoc(state);
+
+	return ret;
+}
+
+/*
+ * Mapping prefixes with namespaces.
+ * Do NOT modify this structure!
+ */
+char * namespace_mapping[] = {"toaster", "http://netconfcentral.org/ns/toaster", NULL, NULL};
+/*
+* CONFIGURATION callbacks
+* Here follows set of callback functions run every time some change in associated part of running datastore occurs.
+* You can safely modify the bodies of all function as well as add new functions for better lucidity of code.
+*/
+
+/**
+ * @brief This callback will be run when node in path /toaster:toaster changes
+ *
+ * @param[in] data	Double pointer to void. Its passed to every callback. You can share data using it.
+ * @param[in] op	Observed change in path. XMLDIFF_OP type.
+ * @param[in] node	Modified node. if op == XMLDIFF_REM its copy of node removed.
+ * @param[out] error	If callback fails, it can return libnetconf error structure with a failure description.
+ *
+ * @return EXIT_SUCCESS or EXIT_FAILURE
+ */
+/* !DO NOT ALTER FUNCTION SIGNATURE! */
+int callback_toaster_toaster (void ** data, XMLDIFF_OP op, xmlNodePtr node, struct nc_err** error)
+{
+	pthread_mutex_lock(&status->toaster_mutex);
+
+	if (op <= 0 || op > (XMLDIFF_MOD | XMLDIFF_CHAIN | XMLDIFF_ADD | XMLDIFF_REM) || ((op & XMLDIFF_ADD) && (op & XMLDIFF_REM))) {
+		*error = nc_err_new(NC_ERR_OP_FAILED);
+		nc_err_set(*error, NC_ERR_PARAM_MSG, "Invalid configuration data modification for toaster module.");
+		return (EXIT_FAILURE);
+	} else {
+		if (op & XMLDIFF_MOD) {/* some child(s) was changed and has no callback */
+			fprintf (stderr, "Node was modified.\n");
+			/* TODO: process the change */
+		}
+
+		if (op & XMLDIFF_CHAIN) { /* some child(s) was changed and it was processed by its callback */
+			fprintf (stderr, "Child(s) of node was modified.\n");
+			/* TODO: finalize children operation */
+		}
+
+		if (op & XMLDIFF_REM) {
+			status->enabled = 0;
+			if (status->toasting != 0) {
+				fprintf (stderr, "Interrupting ongoing toasting!\n");
+				status->toasting = 0;
+			}
+		} else if (op & XMLDIFF_ADD) {
+			status->enabled = 1;
+		}
+	}
+
+	fprintf (stderr, "Turning toaster %s\n", status->enabled ? "on" : "off");
+
+	pthread_mutex_unlock(&status->toaster_mutex);
+
+	return EXIT_SUCCESS;
+}
+
+/*
+* Structure transapi_config_callbacks provide mapping between callback and path in configuration datastore.
+* It is used by libnetconf library to decide which callbacks will be run.
+* DO NOT alter this structure
+*/
+struct transapi_xml_data_callbacks clbks =  {
+	.callbacks_count = 1,
+	.data = NULL,
+	.callbacks = {
+		{.path = "/toaster:toaster", .func = callback_toaster_toaster}
+	}
+};
+
+/*
+* RPC callbacks
+* Here follows set of callback functions run every time RPC specific for this device arrives.
+* You can safely modify the bodies of all function as well as add new functions for better lucidity of code.
+* Every function takes array of inputs as an argument. On few first lines they are assigned to named variables. Avoid accessing the array directly.
+* If input was not set in RPC message argument in set to NULL.
+*/
+
+void * make_toast (void * doneness)
+{
+	/* pretend toasting */
+	sleep (*(int*)doneness);
+
+	pthread_mutex_lock (&status->toaster_mutex);
+
+	if (status->toasting == 0) { /* was canceled */
+		pthread_mutex_unlock (&status->toaster_mutex);
+	} else { /* still toasting */
+		/* turn off */
+		status->toasting = 0;
+		ncntf_event_new(-1, NCNTF_GENERIC, "<toastDone><toastStatus>done</toastStatus></toastDone>");
+	}
+
+	pthread_mutex_unlock (&status->toaster_mutex);
+	return NULL;
+}
+
+nc_reply * rpc_make_toast (xmlNodePtr input[])
+{
+	xmlNodePtr toasterDoneness = input[0];
+	xmlNodePtr toasterToastType = input[1];
+
+	struct nc_err * err;
+	nc_reply * reply;
+	int doneness; 
+	pthread_t tid;
+
+	if (toasterDoneness == NULL) { /* doneness not specified, use default*/
+		doneness = 5;
+	} else { /* get doneness value */
+		doneness = atoi (xmlNodeGetContent(toasterDoneness));
+	}
+
+	pthread_mutex_lock(&status->toaster_mutex);
+
+	if (status->enabled == 0) { /* toaster is off */
+		reply = nc_reply_error(nc_err_new (NC_ERR_RES_DENIED));
+	} else if (status->toasting) { /* toaster is busy */
+		reply = nc_reply_error (nc_err_new (NC_ERR_IN_USE));
+	} else if (doneness == 0) { /* doneness must be from <1,10> */
+		reply = nc_reply_error (nc_err_new (NC_ERR_INVALID_VALUE));
+	} else if (pthread_create (&tid, NULL, make_toast, &doneness)) { /* toaster internal error (cannot turn heater on) */
+			err = nc_err_new (NC_ERR_OP_FAILED);
+			nc_err_set (err, NC_ERR_PARAM_MSG, "Toaster is broken!");
+			ncntf_event_new(-1, NCNTF_GENERIC, "<toastDone><toastStatus>error</toastStatus></toastDone>");
+			reply = nc_reply_error (err);
+	} else { /* all ok, start toasting */
+		status->toasting = 1;
+		reply = nc_reply_ok();
+		pthread_detach (tid);
+	}
+
+	pthread_mutex_unlock(&status->toaster_mutex);
+	return reply;
+}
+
+nc_reply * rpc_cancel_toast (xmlNodePtr input[])
+{
+	nc_reply * reply;
+	struct nc_err * err;
+
+	pthread_mutex_lock(&status->toaster_mutex);
+
+	if (status->enabled == 0) {/* toaster is off */
+		reply = nc_reply_error(nc_err_new (NC_ERR_RES_DENIED));
+	} else if (status->toasting == 0) { /* toaster in not toasting */
+		err = nc_err_new (NC_ERR_OP_FAILED);
+		nc_err_set (err, NC_ERR_PARAM_MSG, "There is no toasting in progress.");
+		reply = nc_reply_error(err);
+	} else { /* interrupt toasting */
+		status->toasting = 0;
+		ncntf_event_new(-1, NCNTF_GENERIC, "<toastDone><toastStatus>canceled</toastStatus></toastDone>");
+		reply = nc_reply_ok();
+	}
+
+	pthread_mutex_unlock(&status->toaster_mutex);
+
+	return reply;
+}
+/*
+* Structure transapi_rpc_callbacks provide mapping between callbacks and RPC messages.
+* It is used by libnetconf library to decide which callbacks will be run when RPC arrives.
+* DO NOT alter this structure
+*/
+struct transapi_xml_rpc_callbacks rpc_clbks = {
+	.callbacks_count = 2,
+	.callbacks = {
+		{.name="make-toast", .func=rpc_make_toast, .arg_count=2, .arg_order={"toasterDoneness", "toasterToastType"}},
+		{.name="cancel-toast", .func=rpc_cancel_toast, .arg_count=0, .arg_order={}}
+	}
+};
+
