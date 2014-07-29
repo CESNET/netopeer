a9a057f0333a2bbf73067230e4501e8aee8a9f28
Radek Krejci <rkrejci@cesnet.cz>
2014-07-29 15:27:14 +0200

    cfgsystem: CHANGE: cfgsystem-init tool
    New tool to prepare startup configuration for the cfgsystem transapi
    module. It is integrated into the install process so it is automatically
    run with 'make install'.

---
+++ transAPI/cfgsystem/cfgsystem-init.c
@@ -0,0 +1,185 @@
+/**
+ * \file cfgsystem-init.c
+ * \brief Startup datastore initiation for cfgsystem transAPI module.
+ * \author Michal Vasko <mvasko@cesnet.cz>
+ * \author Radek Krejci <rkrejci@cesnet.cz>
+ * \date 2013
+ * \date 2014
+ *
+ * Copyright (C) 2013-2014 CESNET
+ *
+ * LICENSE TERMS
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of the Company nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * ALTERNATIVELY, provided that this notice is retained in full, this
+ * product may be distributed under the terms of the GNU General Public
+ * License (GPL) version 2 or later, in which case the provisions
+ * of the GPL apply INSTEAD OF those given above.
+ *
+ * This software is provided ``as is'', and any express or implied
+ * warranties, including, but not limited to, the implied warranties of
+ * merchantability and fitness for a particular purpose are disclaimed.
+ * In no event shall the company or contributors be liable for any
+ * direct, indirect, incidental, special, exemplary, or consequential
+ * damages (including, but not limited to, procurement of substitute
+ * goods or services; loss of use, data, or profits; or business
+ * interruption) however caused and on any theory of liability, whether
+ * in contract, strict liability, or tort (including negligence or
+ * otherwise) arising in any way out of the use of this software, even
+ * if advised of the possibility of such damage.
+ *
+ */
+
+#include <stdio.h>
+#include <dlfcn.h>
+#include <libxml/tree.h>
+
+#include <libnetconf.h>
+
+/* from cfgsystem.c */
+int transapi_init(xmlDocPtr *running);
+
+void my_print(NC_VERB_LEVEL level, const char* msg)
+{
+	switch (level) {
+	case NC_VERB_ERROR:
+		fprintf(stderr, "ERROR: %s\n", msg);
+		break;
+	case NC_VERB_WARNING:
+		fprintf(stderr, "WARNING: %s\n", msg);
+		break;
+	case NC_VERB_VERBOSE:
+		fprintf(stderr, "VERBOSE: %s\n", msg);
+		break;
+	case NC_VERB_DEBUG:
+		fprintf(stderr, "DEBUG: %s\n", msg);
+		break;
+	}
+}
+
+void help(const char* progname)
+{
+	fprintf(stdout, "Usage: %s path\n\n", progname);
+	fprintf(stdout, "  path    Path to the cfgsystem's datastore file.\n\n");
+}
+
+void create_datastore(xmlDocPtr *datastore, xmlNodePtr *startup)
+{
+	xmlNodePtr root, node;
+	xmlNsPtr ns;
+
+	*datastore = xmlNewDoc(BAD_CAST "1.0");
+	root = xmlNewNode(NULL, BAD_CAST "datastores");
+	xmlDocSetRootElement(*datastore, root);
+	ns = xmlNewNs(root, BAD_CAST "urn:cesnet:tmc:datastores:file", NULL);
+	xmlSetNs(root, ns);
+
+	node = xmlNewChild(root, root->ns, BAD_CAST "running", NULL);
+	xmlNewProp(node, BAD_CAST "lock", BAD_CAST "");
+	node =xmlNewChild(root, root->ns, BAD_CAST "candidate", NULL);
+	xmlNewProp(node, BAD_CAST "lock", BAD_CAST "");
+	xmlNewProp(node, BAD_CAST "modified", BAD_CAST "false");
+	*startup = node = xmlNewChild(root, root->ns, BAD_CAST "startup", NULL);
+	xmlNewProp(node, BAD_CAST "lock", BAD_CAST "");
+}
+
+int main(int argc, char** argv)
+{
+	xmlDocPtr startup_doc = NULL, datastore = NULL;
+	xmlNodePtr startup_node = NULL, node, rm;
+	int ret = 0;
+
+	if (argc != 2 || argv[1][0] == '-') {
+		help(argv[0]);
+		return 1;
+	}
+
+	/* init libnetconf for messages  from transAPI function */
+	if (nc_init(0) == -1) {
+		my_print(NC_VERB_ERROR, "Could not initialize libnetconf.");
+		return 1;
+	}
+
+	/* set message printing callback */
+	nc_callback_print(my_print);
+
+	if (transapi_init(&startup_doc) != EXIT_SUCCESS) {
+		nc_close(0);
+		return 1;
+	}
+
+	if (startup_doc == NULL || startup_doc->children == NULL) {
+		/* nothing to do */
+		nc_close(0);
+		return 0;
+	}
+
+	/* get datastore */
+	datastore = xmlReadFile(argv[1], NULL, XML_PARSE_NOERROR | XML_PARSE_NOWARNING | XML_PARSE_NOBLANKS | XML_PARSE_NSCLEAN);
+	if (datastore != NULL) {
+		/* get the startup node */
+		for (node = datastore->children; node != NULL; node = node->next) {
+			if (node->type != XML_ELEMENT_NODE || xmlStrcmp(node->name, BAD_CAST "datastores") != 0) {
+				continue;
+			}
+			for (node = node->children; node != NULL; node = node->next) {
+				if (node->type != XML_ELEMENT_NODE || xmlStrcmp(node->name, BAD_CAST "startup") != 0) {
+					continue;
+				}
+				startup_node = node;
+				break;
+			}
+			break;
+		}
+
+		if (startup_node == NULL) {
+			/* datastore is corrupted, create a new one */
+			xmlFreeDoc(datastore);
+			create_datastore(&datastore, &startup_node);
+		}
+	} else {
+		/* dotastore does not exists or it is corrupted, create a new one */
+		create_datastore(&datastore, &startup_node);
+	}
+
+	if (startup_node == NULL) {
+		nc_verb_error("Unable to process datastore file content.");
+		nc_close(0);
+		return 1;
+	}
+
+	/* cleanup startup */
+	for (node = startup_node->children; node != NULL; ) {
+		rm = node;
+		node = node->next;
+		xmlUnlinkNode(rm);
+		xmlFreeNode(rm);
+	}
+
+	/* reconnect startup config from transapi_init into the datastore */
+	node = xmlCopyNodeList(startup_doc->children);
+	xmlAddChildList(startup_node, node);
+
+	xmlFreeDoc(startup_doc);
+
+	if (xmlSaveFormatFile(argv[1], datastore, 1) == -1) {
+		nc_verb_error("Unable to store dtastore to \"%s\" file.", argv[1]);
+		ret = 1;
+	}
+	xmlFreeDoc(datastore);
+
+	nc_close(0);
+	return ret;
+}
