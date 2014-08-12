cc1fa81fb7561e5adbe931acdba7acc66d9d7c74
Radek Krejci <rkrejci@cesnet.cz>
2014-08-12 10:22:10 +0200

    configurator: BUGFIX: remove note about --with-user
    Remove note about configure's option that was already removed.

--- server/configurator/netopeer/nc_sock.py
+++ server/configurator/netopeer/nc_sock.py
@@ -24,9 +24,10 @@ class nc_sock(ncmodule.ncmodule):
 		try:
 			window.addstr('For intercommunication between Netopeer server and agents is used: UNIX sockets\n\n')
 
-			window.addstr('To change the values below, recompile the Netopeer server\nwith the configure options --with-user and --with-group.\n\n')
 			window.addstr('Allowed user to start the Netopeer server:\n')
 			window.addstr('  {s}\n\n'.format(s=config.options['user']))
+
+			window.addstr('To change the value below, recompile the Netopeer server\nwith the configure options --with-group.\n\n')
 
 			if config.options['group'] == '':
 				window.addstr('All users are allowed to connect to the Netopeer server.\n')
