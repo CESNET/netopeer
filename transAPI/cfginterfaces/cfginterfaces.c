38f954316bf0bd33f7b8fb0b80323c4ab36430e4
Michal Vasko <mvasko@cesnet.cz>
2014-11-03 11:04:40 +0100

    cfginterfaces: CHANGE: transAPI module version updated

--- transAPI/cfginterfaces/cfginterfaces.c
+++ transAPI/cfginterfaces/cfginterfaces.c
@@ -15,7 +15,7 @@
 #include "config.h"
 
 /* transAPI version which must be compatible with libnetconf */
-int transapi_version = 4;
+int transapi_version = 5;
 
 /* Signal to libnetconf that configuration data were modified by any callback.
  * 0 - data not modified
