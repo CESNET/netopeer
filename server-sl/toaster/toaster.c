90a37daef53cbea4bb107ed58c08cca41cf24d34
Radek Krejci <rkrejci@cesnet.cz>
2014-11-07 11:38:06 +0100

    toaster: UPDATE: move toaster module to transAPI v5

--- server-sl/toaster/toaster.c
+++ server-sl/toaster/toaster.c
@@ -16,7 +16,7 @@
 #include <sys/shm.h>
 
 /* transAPI version which must be compatible with libnetconf */
-int transapi_version = 4;
+int transapi_version = 5;
 
 /*
  * Signal to libnetconf that configuration data were modified by any callback.
