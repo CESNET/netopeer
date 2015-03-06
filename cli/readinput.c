f34100dac7268470300269e7f6a0cd94bd96e408
Michal Vasko <mvasko@cesnet.cz>
2015-03-06 12:11:34 +0100

    cli: FEATURE: use external editor for manual XML editing

--- cli/readinput.c
+++ cli/readinput.c
@@ -1,10 +1,10 @@
 /*
- * mreadline.c
- * Author Radek Krejci <rkrejci@cesnet.cz>
+ * readinput.c
+ * Author Radek Krejci <rkrejci@cesnet.cz>, Michal Vasko <mvasko@cesnet.cz>
  *
- * Modification of the libreadline functions for NETCONF client.
+ * Reading input using an external editor for NETCONF client.
  *
- * Copyright (C) 2012 CESNET, z.s.p.o.
+ * Copyright (C) 2015 CESNET, z.s.p.o.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -37,25 +37,35 @@
  * if advised of the possibility of such damage.
  *
  */
+#define _GNU_SOURCE
+#define _BSD_SOURCE
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <readline/readline.h>
 
 #include "commands.h"
 
+#define EDITOR_DEFAULT "vi"
+#define EDITOR_ENV "NETOPEER_CLI_EDITOR"
+
 static const char rcsid[] __attribute__((used)) ="$Id: "__FILE__": "RCSID" $";
+
+extern char* config_editor;
 
 extern COMMAND commands[];
 extern char* cert_commands[];
 extern char* crl_commands[];
-volatile int multiline = 0;
 
 /* Generator function for command completion.  STATE lets us know whether
  to start from scratch; without any state (i.e. STATE == 0), then we
  start at the top of the list. */
-char * cmd_generator(const char *text, int state)
-{
+char* cmd_generator(const char* text, int state) {
 	static int list_index, len;
 	char *name;
 
@@ -80,8 +90,7 @@ char * cmd_generator(const char *text, int state)
 	return ((char *) NULL);
 }
 
-char* subcmd_generator(const char* text, int state)
-{
+char* subcmd_generator(const char* text, int state) {
 	static int list_index, len;
 	static char** cmds = NULL;
 	char *name;
@@ -123,11 +132,10 @@ char* subcmd_generator(const char* text, int state)
  *
  * \return The array of matches, or NULL if there aren't any.
  */
-char ** cmd_completion(const char *text, int start, int end)
-{
-	char **matches;
+char** cmd_completion(const char* text, int start, int end) {
+	char** matches;
 
-	matches = (char **) NULL;
+	matches = (char**) NULL;
 
 	/* If this word is at the start of the line, then it is a command
 	 to complete.  Otherwise it is the name of a file in the current
@@ -142,56 +150,118 @@ char ** cmd_completion(const char *text, int start, int end)
 	return (matches);
 }
 
-int my_bind_cr(int UNUSED(count), int UNUSED(key))
-{
-	  if (multiline == 0) {
-		  rl_point = rl_end;
-		  rl_redisplay();
-		  rl_done = 1;
-	  }
-	  printf("\n");
-
-	  return (0);
-}
-
-int my_bind_esc(int UNUSED(count), int UNUSED(key))
-{
-	  if (multiline == 1) {
-		  rl_point = rl_end;
-		  rl_redisplay();
-		  rl_done = 1;
-		  printf("\n");
-	  }
-	  return (0);
-}
-
 /**
  * \brief Tell the GNU Readline library how to complete commands.
  *
  * We want to try to complete on command names if this is the first word in the
  * line, or on filenames if not.
  */
-void initialize_readline()
-{
+void initialize_readline(void) {
 	/* Allow conditional parsing of the ~/.inputrc file. */
 	rl_readline_name = "netconf";
 
 	/* Tell the completer that we want a crack first. */
 	rl_attempted_completion_function = cmd_completion;
-
-	rl_bind_key('\n', my_bind_cr);
-	rl_bind_key('\r', my_bind_cr);
-	rl_bind_key('D' - 64, my_bind_esc); /* Ctrl-D */
 }
 
-char *mreadline(const char * prompt)
-{
-	char* ret;
+/* every new line must begin with '#' (except the first) in the instruction not to be confused with the input */
+char* readinput(const char* instruction) {
+	int fd = -1, ret, size;
+	pid_t pid, wait_pid;
+	char* tmpname = NULL, *input = NULL;
+	const char* editor = NULL;
 
-	multiline = 1; /* enable multiline editing */
-	ret = readline(prompt);
-	multiline = 0; /* disable multiline editing */
+	editor = getenv(EDITOR_ENV);
+	if (editor == NULL) {
+		editor = config_editor;
+	}
+	if (editor == NULL) {
+		editor = getenv("EDITOR");
+	}
+	if (editor == NULL) {
+		editor = EDITOR_DEFAULT;
+	}
 
-	return (ret);
+	asprintf(&tmpname, "/tmp/tmpXXXXXX.xml");
+
+	fd = mkstemps(tmpname, 4);
+	if (fd == -1) {
+		ERROR("readinput", "Failed to create a temporary file (%s).", strerror(errno));
+		goto fail;
+	}
+
+	if (instruction != NULL) {
+		ret = write(fd, "\n<!--\n", 6);
+		ret += write(fd, instruction, strlen(instruction));
+		ret += write(fd, "\n-->\n", 5);
+		if (ret < 6+strlen(instruction)+5) {
+			ERROR("readinput", "Failed to write the instruction (%s).", strerror(errno));
+			goto fail;
+		}
+
+		ret = lseek(fd, 0, SEEK_SET);
+		if (ret == -1) {
+			ERROR("readinput", "Rewinding the temporary file failed (%s).", strerror(errno));
+			goto fail;
+		}
+	}
+
+	if ((pid = vfork()) == -1) {
+		ERROR("readinput", "Fork failed (%s).", strerror(errno));
+		goto fail;
+	} else if (pid == 0) {
+		/* child */
+		execlp(editor, editor, tmpname, (char*)NULL);
+
+		ERROR("readinput", "Exec failed (%s).", strerror(errno));
+		exit(1);
+	} else {
+		/* parent */
+		wait_pid = wait(&ret);
+		if (wait_pid != pid) {
+			ERROR("readinput", "Child process other than the editor exited, weird.");
+			goto fail;
+		}
+		if (!WIFEXITED(ret)) {
+			ERROR("readinput", "Editor exited in a non-standard way.");
+			goto fail;
+		}
+	}
+
+	/* Get the size of the input */
+	size = lseek(fd, 0, SEEK_END);
+	if (size == -1) {
+		ERROR("readinput", "Failed to get the size of the temporary file (%s).", strerror(errno));
+		goto fail;
+	}
+	lseek(fd, 0, SEEK_SET);
+
+	input = malloc(size+1);
+	input[size] = '\0';
+
+	/* Read the input */
+	ret = read(fd, input, size);
+	if (ret < size) {
+		ERROR("readinput", "Failed to read from the temporary file (%s).", strerror(errno));
+		goto fail;
+	}
+
+	/* Clean the temporary file stuff */
+	close(fd);
+	fd = -1;
+	unlink(tmpname);
+	free(tmpname);
+	tmpname = NULL;
+
+	return input;
+
+fail:
+	close(fd);
+	if (tmpname != NULL) {
+		unlink(tmpname);
+	}
+	free(tmpname);
+	free(input);
+
+	return NULL;
 }
-
