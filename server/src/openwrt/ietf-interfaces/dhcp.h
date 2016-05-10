3daf2c143eea326367ef29c1d6ba811a492e96d5
Peter Nagy <xnagyp01@stud.fit.vutbr.cz>
2016-05-10 09:25:59 +0200

    ietf-interfaces: dhcpv4 server via dnsmasq.conf

--- server/src/openwrt/ietf-interfaces/dhcp.h
+++ server/src/openwrt/ietf-interfaces/dhcp.h
@@ -47,6 +47,7 @@
 #include "../ietf-system/dns_resolver.h"
 
 int iface_ipv4_origin(const char* if_name, unsigned char origin, XMLDIFF_OP op, char** msg);
+int dhcp_ipv4_server(char* start, char* stop, char* leasetime, char* default_gateway, XMLDIFF_OP op, char** msg);
 char* dhcp_get_ipv4_default_gateway(const char* if_name, char** msg);
 char** dhcp_get_dns_server(char** msg);
 char** dhcp_get_dns_search(char** msg);
